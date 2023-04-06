84aeeba93a920c0b861f318d085cc99fea5e9006
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-04-06 22:21:57 +0900

    commit

--- Sample-TableView/Controller/TableViewController.swift
+++ Sample-TableView/Controller/TableViewController.swift
@@ -12,7 +12,7 @@ class TableViewController: UITableViewController {
     private var items: [Article] = []
     let articlesAPIClient = ArticleAPIClient()
     let indicatotor = UIActivityIndicatorView()
-    var sampleImage: UIImage?
+    let indicatotor2 = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,26 +30,38 @@ class TableViewController: UITableViewController {
     
     //Section（ない場合は1が帰る）
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
-
+    
     //cellの行数
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return items.count
     }
-
+    
     //cellの生成
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-
-//        var configulation = cell.defaultContentConfiguration()
-//        configulation.text = items[indexPath.row].title
-//        cell.contentConfiguration = configulation
         
+//        var content = cell.defaultContentConfiguration()
+//        content.text = items[indexPath.row].title
         cell.textLabel?.text = items[indexPath.row].title
-        cell.imageView?.kf.setImage(with: items[indexPath.row].user.profileImageURL)
         
+        URLSession.shared.dataTask(with: items[indexPath.row].user.profileImageURL) { (data, _, error) in
+            if let error = error {
+                print(error)
+            } else {
+                if let data = data {
+                    let image = UIImage(data: data)
+                    DispatchQueue.main.async {
+                        cell.imageView?.image = image
+//                        content.image = image
+//                        cell.contentConfiguration = content
+                        cell.setNeedsLayout()
+                    }
+                }
+            }
+        }.resume()
         return cell
     }
     
